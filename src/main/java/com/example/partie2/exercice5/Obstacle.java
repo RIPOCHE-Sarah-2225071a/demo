8ff1cba155af5fb28af5fd47446c28cc92eb91c4
Sarah <sarah.ripoche@etu.univ-amu.fr>
2023-05-10 12:13:36 +0200

    Exercice 1 pas fini

---
+++ src/main/java/com/example/partie2/exercice5/Obstacle.java
@@ -0,0 +1,11 @@
+package com.example.partie2.exercice5;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Obstacle extends Rectangle {
+    public Obstacle(double x, double y, double width, double height){
+        super(x, y, width, height);
+        setFill(Color.BLUE);
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,4 +9,5 @@ module com.example.demo {
     exports com.example.demo;
     exports com.example.partie1;
     exports com.example.partie2;
+    exports com.example.partie2.exercice5;
 }
